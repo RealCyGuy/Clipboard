a657e10a1983ef0de7b5d2609a7703ce4f7dd617
Cyrus Yip <cyruscmyip1@gmail.com>
2020-03-16 23:36:01 -0700

    Help command changes
    - remove old help command
    - list view formats correctly

--- cogs/misc.py
+++ cogs/misc.py
@@ -14,32 +14,6 @@ class Misc(commands.Cog):
         else:
             return ctx.prefix
 
-    # @commands.group(invoke_without_command=True)
-    # async def help(self, ctx):
-    #     help_embed = discord.Embed(title='Command Categories', colour=0x56f795)
-    #     help_embed.add_field(name='Clipboard', value=f'**{self.get_used_prefix(ctx)}copy <text>**', inline=True)
-    #     help_embed.add_field(name='Misc', value=f'*Miscellaneous commands.*', inline=True)
-    #     help_embed.set_footer(text=f'Use {self.get_used_prefix(ctx)}help <category> to view the available commands.')
-    #     await ctx.send(embed=help_embed)
-    #
-    # @help.command(name="clipboard")
-    # async def help_clipboard(self, ctx):
-    #     embed = discord.Embed(title='Clipboard commands:', colour=0x56f795)
-    #     embed.description = "Commands related to copying and pasting."
-    #     embed.add_field(name=f'`{self.get_used_prefix(ctx)}copy <text>`', value="Copy some text.", inline=True)
-    #     embed.add_field(name=f'`{self.get_used_prefix(ctx)}paste`', value="Paste some text.", inline=True)
-    #     embed.set_footer(text=f'Use {self.get_used_prefix(ctx)}help to see the categories.')
-    #     await ctx.send(embed=embed)
-    #
-    # @help.command(name="misc")
-    # async def help_misc(self, ctx):
-    #     embed = discord.Embed(title='Miscellaneous commands:', colour=0x56f795)
-    #     embed.description = "Commands that don't really belong anywhere else."
-    #     embed.add_field(name=f'`{self.get_used_prefix(ctx)}help [category]`', value="Shows help.", inline=True)
-    #     embed.add_field(name=f'`{self.get_used_prefix(ctx)}invite`', value="Get the invite link!.", inline=True)
-    #     embed.set_footer(text=f'Use {self.get_used_prefix(ctx)}help to see the categories.')
-    #     await ctx.send(embed=embed)
-
     @commands.command(aliases=['commands'])
     async def help(self, ctx, *, command_name: str = None):
         """
@@ -107,7 +81,7 @@ class Misc(commands.Cog):
             help_embed.set_footer(text=f"Use {self.get_used_prefix(ctx)}help to see all the commands."+" "*30+"\u200b")
         for cog_commands in runnable_commands:
             value = '\n'.join(
-                [f"**{ctx.prefix}{command.qualified_name}** - *{command.short_doc}*" for command in cog_commands])
+                [f"**{ctx.prefix}{command.qualified_name}** - *{command.short_doc.strip()}*" for command in cog_commands])
             value = value.replace(" - **", "")
             help_embed.add_field(
                 name=cog_commands[0].cog_name,
